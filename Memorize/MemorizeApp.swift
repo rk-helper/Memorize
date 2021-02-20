b22d829b68585c63c6c0ec816412db0ab5819bb4
rk-helper <62377740+rk-helper@users.noreply.github.com>
2021-02-20 15:26:26 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rustem on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
